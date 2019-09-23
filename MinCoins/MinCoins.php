a74d8eb3948a0d566979236bd264b4b028a1132b
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-09-23 15:14:32 -0700

    changed logic a bit for in array

--- MinCoins/MinCoins.php
+++ MinCoins/MinCoins.php
@@ -10,19 +10,24 @@ function minNumberOfCoinsNeeded($nums)
     {
         return $coins;
     }
-    elseif ($nums in_array $demonitions 
-    {
+    elseif ($nums == 1 || $nums == 5 || $nums == 10 || $nums == 25) {
         $coins = 1;
         return $coins;
     }
+    /*
+    elseif (in_array(1,5, 10, 25, $demonitions, true)) {
+        $coins = 1;
+        return $coins;
+    }
+    */
     else
     {
+        //will fix syntax later
         $coins = min(1 + minNumberOfCoinsNeeded($nums - $b) for $b in_array $demonitions if $nums - $b >=0);
         return $coins;
     }
 }
 
-
-
+echo $coins;
 ?>
 
